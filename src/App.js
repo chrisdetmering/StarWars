0b85c9c7db05b5fdd043942a71b6a5f9723688d0
froggy <chrisdetmering@gmail.com>
2021-03-09 12:42:04 -0800

    App.js file code copied and pasted

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React, {Component} from 'react';
 import Input from './Input.js';
 import Table from './Table.js';
 import axios from 'axios';
+import ButtonPageChange from './ButtonPageChange.js';
+
+
 
 class App extends Component {
   constructor (props) {
     super(props);
+
     this.state = {
      input : '',
      characters: [],
-     isLoaded: false, 
-     count: 0, 
+     isLoaded: false
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.searchCharacters = this.searchCharacters.bind(this);
-    this.handlePageChange = this.handlePageChange.bind(this); 
-    this.getCharacters = this.getCharacters.bind(this); 
+    this.sendSearchRequest = this.sendSearchRequest.bind(this);
+    this.changePageCount = this.changePageCount.bind(this);
+    this.showPage = this.showPage.bind(this);
+    this.searchCharacter = this.searchCharacter.bind(this);
+    this.resetInputValue = this.resetInputValue.bind(this);
+    
   }
+
+  
+  
+  componentDidMount () {
+    this.showPage(1)
+  } 
 
   showPage(value) {
     try {
@@ -69,6 +81,76 @@ class App extends Component {
       this.sendSearchRequest()  
     }
   }
+
+
+
+
+ sendSearchRequest = 
+  async () => {
+      try{
+        const page1 = await axios.get('https://swapi.dev/api/people/?page=1').then(res => res.data.results)
+        const page2 = await axios.get('https://swapi.dev/api/people/?page=2').then(res => res.data.results)
+        const page3 = await axios.get('https://swapi.dev/api/people/?page=3').then(res => res.data.results)
+        const page4 = await axios.get('https://swapi.dev/api/people/?page=4').then(res => res.data.results)
+        const page5 = await axios.get('https://swapi.dev/api/people/?page=5').then(res => res.data.results)
+        const page6 = await axios.get('https://swapi.dev/api/people/?page=6').then(res => res.data.results)
+        const page7 = await axios.get('https://swapi.dev/api/people/?page=7').then(res => res.data.results)
+        const page8 = await axios.get('https://swapi.dev/api/people/?page=8').then(res => res.data.results)
+        const page9 = await axios.get('https://swapi.dev/api/people/?page=9').then(res => res.data.results)
+        
+    
+        const pages = page1.concat(page2, page3, page4, page5, page6, page7, page8, page9)
+  
+  
+        for(const character of pages) {
+          const homeworldURL = character.homeworld
+          const newHomeworldURL = homeworldURL.replace("http", "https")
+          const homeworldRes = await axios.get(newHomeworldURL).then(res => res.data.name)
+          character.homeworld = [homeworldRes]
+          if(character.species.length !== 0){
+            const speciesURL = character.species;
+            const speciesURLstring = speciesURL.toString()
+            const newSpeciesURL = speciesURLstring.replace("http", "https")
+            const speciesRes = await axios.get(newSpeciesURL).then(res => res.data.name)
+            character.species = [speciesRes]
+          }else{
+            character.species = 'Human'
+          }
+           
+          if(character.name.toLowerCase().startsWith((this.state.input).toLowerCase()) == true && this.state.input !== ''){
+           
+          let characters = this.state.characters;
+          characters.push(character);
+          this.setState({characters: [...characters]})
+        }
+      }   
+        this.setState({isLoaded: true})
+      }catch(err) {
+        console.error(err)
+      }
+ }
+
+
+
+
+  changePageCount(e) {
+    e.preventDefault();
+    if(this.state.isLoaded){
+      this.setState({isLoaded: false})
+    }
+    this.showPage(e.target.value)
+  }
+
+
+
+
+  resetInputValue (e) {
+   e.preventDefault();
+   e.target.value = ''
+  }
+
+
+
 
   render () {
     const {isLoaded} = this.state;
